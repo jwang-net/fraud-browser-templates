107f4e9eb589afe30d3c29af0a3f6f711a6c2884
Jeremy Wang <gcvvvvvv@gmail.com>
2018-07-26 13:55:13 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+[[ $(pwd) = *fraud-browser-templates* ]] || fail "ERROR: build.sh must be run from the root of this directory"
+export ROOT_DIR=`pwd`
+
+function usage {
+    cat <<EOM
+Usage:
+    build.sh (--chrome_version|--firefox_version)
+
+    --chrome_version        Specify chrome version
+    --firefox_version       Specify firefox version
+EOM
+    fail
+}
+
+function fail {
+    >&2 echo "${1}"
+    exit 1
+}
+
+function setup {
+    # TODO: Determine dependencies that the machine running this will need
+    echo "Setting up dependencies..."
+}
+
+function build_docker_image {
+    if [ "${1}" = base ]; then
+        cd base
+        echo "Building Docker base image..."
+        docker build -t fraud-browser-base .
+        cd "${ROOT_DIR}"
+    else
+        cd "${1}"
+        export BROWSER_FILENAME=$(find . -name "${EXTENSION}" | sed 's/^.\///')
+        export IMAGE_NAME="${BROWSER}"_"${BROWSER_VERSION}"
+        echo "Building docker image $IMAGE_NAME with $BROWSER version $BROWSER_VERSION"
+        docker build -t "${IMAGE_NAME}" . --build-arg version="${BROWSER_VERSION}" --build-arg filename="${BROWSER_FILENAME}"
+        cd "${ROOT_DIR}"
+    fi
+}
+
+function main {
+    python get_browser.py "${BROWSER}" "${BROWSER_VERSION}"
+    build_docker_image base
+    build_docker_image "${BROWSER}"
+    docker image ls
+}
+
+for arg in "$@"; do
+    case "${arg}" in
+        --chrome_version=*)
+            export BROWSER_VERSION="${arg#*=}"
+            export BROWSER=chrome
+            export EXTENSION="*.deb"
+            ;;
+        --firefox_version=*)
+            export BROWSER_VERSION="${arg#*=}"
+            export BROWSER=firefox
+            export EXTENSION="*.tar.bz2"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+setup
+main
+cd "${ROOT_DIR}"
---
+++ get_browser.py
@@ -0,0 +1,30 @@
+import boto3
+from botocore.exceptions import ClientError
+import sys
+
+
+s3 = boto3.client('s3')
+bucket = 'fraud-browsers-archive'
+browser = sys.argv[1]
+browser_version = sys.argv[2]
+os = 'linux-x86_64'
+
+
+if browser == 'chrome':
+    key = '{}/{}/chrome64_{}.deb'.format(browser, os, browser_version)
+    local_filename = '{}/chrome64_{}.deb'.format(browser, browser_version)
+elif browser == 'firefox':
+    key = '{}/{}/firefox_{}.tar.bz2'.format(browser, os, browser_version)
+    local_filename = '{}/firefox_{}.tar.bz2'.format(browser, browser_version)
+else:
+    raise Exception('ERROR: Unsupported browser {}.'.format(browser))
+
+
+try:
+    s3.download_file(bucket, key, local_filename)
+    print 'Downloaded {} to {} from bucket {}.'.format(key, local_filename, bucket)
+except ClientError as e:
+    if e.response['Error']['Code'] == '404':
+            print 'Object {} does not exist.'.format(key)
+    else:
+        raise
