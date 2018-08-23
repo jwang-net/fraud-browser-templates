1cd849922747b9fceab4c5c5ca639d963ed74612
Jeremy Wang <gcvvvvvv@gmail.com>
2018-08-23 11:42:23 -0400

    removed entry_point.sh for chrome_simple

--- chrome_simple/entry_point.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone git@github.com:integralads/swarm-js.git
-git clone git@github.com:integralads/charmer.git
-cd /home/swarm-js && pip install -r requirements.txt
-cd /home/charmer
-
-./test-runner --chrome --location=local --test=/home/swarm-js/test/python/component/test_homies.py
