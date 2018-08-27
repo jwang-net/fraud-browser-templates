7142f2a40ae153df1da3739b011081bfc6fa73fa
Jeremy Wang <gcvvvvvv@gmail.com>
2018-08-27 17:40:00 -0400

    Updates to chrome_trimtricks script

--- scripts/chrome_trimtricks.sh
+++ scripts/chrome_trimtricks.sh
@@ -11,9 +11,9 @@ source build.sh --simple --chrome_version="$@"
 
 # execute additional stuff on container
 CONTAINER_ID=$(docker run -it -d "${IMAGE_NAME}" /bin/bash)
-docker exec -it "${CONTAINER_ID}" cd /home/pyvault && python setup.py install
-docker exec -it "${CONTAINER_ID}" cd /home/charmer && python setup.py install
-docker exec -it "${CONTAINER_ID}" cd /home/swarm-js && cat requirements.txt | xargs -n 1 pip install
+docker exec "${CONTAINER_ID}" cd /home/pyvault && python setup.py install
+docker exec "${CONTAINER_ID}" cd /home/charmer && python setup.py install
+docker exec "${CONTAINER_ID}" cd /home/swarm-js && cat requirements.txt | xargs -n 1 pip install
 
 # run tests
-docker exec -it "${CONTAINER_ID}" cd /home/charmer && ./test-runner --location=local --chrome --test=/home/swarm-js/test/python/component/test_homies.py
+docker exec "${CONTAINER_ID}" cd /home/charmer && ./test-runner --location=local --chrome --test=/home/swarm-js/test/python/component/test_homies.py
