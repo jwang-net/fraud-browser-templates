813630cb62aac42e529b4eabb8f9b5f2374d0382
Jeremy Wang <gcvvvvvv@gmail.com>
2018-08-28 11:54:30 -0400

    add chrome_trimtricks.sh script and updates to firefox_simple dockerfile

--- scripts/chrome_trimtricks.sh
+++ scripts/chrome_trimtricks.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/bin/bash -l
 
 # get test repos
 source "${WORKSPACE}"/cleanup.sh
 cd "${WORKSPACE}"/chrome_simple
 git clone git@github.com:integralads/pyvault.git
-git clone git@github.com:integralads/swarm-js.git
-git clone git@github.com:integralads/charmer.git
+git clone -b SAD-4717 git@github.com:integralads/swarm-js.git
+git clone -b SAD-4717 --single-branch git@github.com:jwang-net/charmer.git
 cd "${WORKSPACE}"
-source build.sh --simple --chrome_version="$@"
+source build.sh --simple --chrome_version=68.0.3440.87
 
 # execute additional stuff on container
-CONTAINER_ID=$(docker run -i -d "${IMAGE_NAME}" /bin/bash)
-docker exec "${CONTAINER_ID}" cd /home/pyvault && python setup.py install
-docker exec "${CONTAINER_ID}" cd /home/charmer && python setup.py install
-docker exec "${CONTAINER_ID}" cd /home/swarm-js && cat requirements.txt | xargs -n 1 pip install
+CONTAINER_ID=$(docker run -t -d "${IMAGE_NAME}" /bin/bash)
+docker exec "${CONTAINER_ID}" bash -c "cd /home/charmer && cat requirements.txt | xargs -n 1 pip install && python setup.py install"
+docker exec "${CONTAINER_ID}" bash -c "cd /home/pyvault && python setup.py install"
+docker exec "${CONTAINER_ID}" bash -c "cd /home/swarm-js && cat requirements.txt | xargs -n 1 pip install"
 
 # run tests
-docker exec "${CONTAINER_ID}" cd /home/charmer && ./test-runner --location=local --chrome --test=/home/swarm-js/test/python/component/test_homies.py
+docker exec "${CONTAINER_ID}" bash -c "cd /home/charmer && ./test-runner --location=local --chrome --test=/home/swarm-js/test/python/component/test_homies.py"
